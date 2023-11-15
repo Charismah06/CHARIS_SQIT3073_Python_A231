27d68f28958adf37a0e9244939b43327ff9c5b86
Charismah06 <148172391+Charismah06@users.noreply.github.com>
2023-11-15 15:59:09 +0800

    Add files via upload

---
+++ CHARIS_SQIT3073_Python_A231/W3_String.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('cls') 
+
+
+# Get user input for integer and floating-point numbers
+a = float(input("Enter a number (a): "))
+b = float(input("Enter another number (b): "))
+c = int(input("Enter an integer (c): "))
+
+# Perform arithmetic operations
+sum_ab = a + b
+difference_ac = a - c
+product_bc = b * c
+quotient_ab = a / b
+modulus_ab = a % b
+exponentiation_ab = a ** b
+
